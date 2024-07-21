9e2b57afe82a3aaa62195aa1945c3b14af49b12e
sos958 <oshadha1xbet1234@gmail.com>
2024-07-21 17:50:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=qJEySBJb#J18NEKkvipQAXtIDsM5KdiuWS0li-bYfYDcK9eVT1bg' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=GQ9iTKKA#xUOjjetZjvJgZi3LouS9and1OHt_OQrusik-2uAtKDE' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
