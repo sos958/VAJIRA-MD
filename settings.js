262b37b8b26e4dc70e5f36eca2cf65c2db8bff76
sos958 <oshadha1xbet1234@gmail.com>
2024-07-21 18:17:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,10 +8,10 @@ function convertToBool(text, fault = 'true') {
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
 const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
+global.owner = process.env.OWNER_NUMBER  || '94705533794' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=GQ9iTKKA#xUOjjetZjvJgZi3LouS9and1OHt_OQrusik-2uAtKDE' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=qBkTTZ6A#DRFblbED_TPdtAljdJDS2jJhsIbI9KEwDgTfI-ycPO0' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
